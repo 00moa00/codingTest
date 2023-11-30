381bd4eed8bb328c2ed34d8e70c50ae53778e0cb
ji young park <moa00_@naver.com>
2023-11-30 20:26:42 +0900

    백준 1159

--- 1129/1129/1129.cpp
+++ 1129/1129/1129.cpp
@@ -4,19 +4,34 @@
 #include <iostream>
 #include <algorithm>
 
-std::string s, temp;
 int main() 
 {
-    std::cin >> s;
-    temp = s;
-    reverse(temp.begin(), temp.end());
-    if (temp == s)
+    int memberCount = 0;    //멤버수
+    int cnt[26] = {0,};            //알파벳
+    std::string memberName;          //멤버 이름
+    std::string ret;
+
+    std::cin >> memberCount;
+    for (int i = 0; i < memberCount; i++)
     {
-        std::cout << 1 << "\n";
+        std::cin >> memberName;
+        cnt[memberName[0] - 'a'] ++;    //첫글자만 필요
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (cnt[i] >= 5)
+        {
+            ret += i + 'a';
+        }
+    }
+
+    if (ret.size())
+    {
+        std::cout << ret << "\n";
     }
     else
     {
-        std::cout << 0 << "\n";
+        std::cout << "PREDAJA" << "\n";
     }
-    return 0;
 }
