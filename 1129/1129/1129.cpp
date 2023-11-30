fadb783ea73386256e5c16ee848b8a5a703a0f1e
ji young park <moa00_@naver.com>
2023-11-30 19:45:41 +0900

    백준 2979

--- 1129/1129/1129.cpp
+++ 1129/1129/1129.cpp
@@ -2,22 +2,31 @@
 //
 
 #include <iostream>
-#include<algorithm>
-#include <vector>
 
 using namespace std;
-string str;
-int cnt[26];
+int A, B, C, a, b, cnt[104], ret;
 
 int main() {
-	cin >> str;
-	for (char a : str) {
-		cnt[a - 'a']++;
-	}
-	for (int i = 0; i < 26; i++)
+	cin >> A >> B >> C; //주차요금
+	for (int i = 0; i < 3; i++) 
 	{
-		cout << cnt[i] << " ";
+		cin >> a >> b;	//도착, 떠난 시간. 이상 미만
+		for (int j = a; j < b; j++)
+		{
+			cnt[j]++;
+		}
 	}
 
+	for (int j = 1; j < 100; j++) 
+	{
+		//트럭이 도착했다면
+		if (cnt[j]) 
+		{
+			if (cnt[j] == 1) ret += A;
+			else if (cnt[j] == 2)ret += B * 2;
+			else if (cnt[j] == 3)ret += C * 3;
+		}
+	}
+	cout << ret << "\n";
 	return 0;
 }
