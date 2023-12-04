3d5353c7d57425befbc36baddac29058f7ba14d2
ji young park <moa00_@naver.com>
2023-12-04 18:26:51 +0900

    1992번 - 쿼드트리

--- 1129/1129/1129.cpp
+++ 1129/1129/1129.cpp
@@ -10,70 +10,51 @@
 #include <tuple>
 
 using namespace std;
+typedef long long int ll;
+int n;
+string s;
+char a[101][101];
 
-int a[101][101], e[101][101], visited[101][101], n, m, k, temp, inputLX, inputLY, inputRX, inputRY;
-int dy[4] = { -1, 0, 1, 0 }, dx[4] = { 0, 1, 0, -1 };
-vector<int> ret;
-
-void dfs(int y, int x) 
+string quard(int y, int x, int size)
 {
-    visited[y][x] = 1;
-    ret.back() += 1;
+    
+    if (size == 1) return string(1, a[y][x]);
 
-    for (int i = 0; i < 4; i++) 
+    char b = a[y][x];
+    string ret = "";
+    for (int i = y; i < y + size; i++) 
     {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-
-        if (ny < 0 || ny >= m || nx < 0 || nx >= n || visited[ny][nx] == 1) continue;
-        if (a[ny][nx] == 1) continue;
-
-        dfs(ny, nx);
-    }
-
-    return;
-}
-
-int main() {
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> m >> n >> k;
-
-    for (int i = 0; i < k; i++)
-    {
-        cin >> inputLX >> inputLY >> inputRX >> inputRY;
-
-        for (int x = inputLX; x < inputRX; x++)
+        for (int j = x; j < x + size; j++) 
         {
-            for (int y = inputLY; y < inputRY; y++)
+            if (b != a[i][j])
             {
-                a[y][x] = 1;
+                ret += '(';
+                ret += quard(y, x, size / 2);     //왼쪽 위
+                ret += quard(y, x + size / 2, size / 2);            //오른쪽 위
+                ret += quard(y + size / 2, x, size / 2);            //왼쪽 아래
+                ret += quard(y + size / 2, x + size / 2, size / 2); //오른쪽 아래
+                ret += ')';
+
+                return ret;
             }
         }
     }
+    return string(1, a[y][x]);
+}
+int main() 
+{
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    cin >> n;
 
-
-    for (int i = 0; i < m; i++) 
+    for (int i = 0; i < n; i++) 
     {
+        cin >> s;
+
         for (int j = 0; j < n; j++) 
         {
-            if (a[i][j] != 1 && visited[i][j] == 0)
-            {
-                ret.push_back(0);
-                dfs(i, j);
-            }
+            a[i][j] = s[j];
         }
     }
-
-    cout<< ret.size() << '\n';
-    sort(ret.begin(), ret.end());
-
-    for (int i = 0; i < ret.size(); i++)
-    {
-        cout << ret[i] << ' ';
-    }
+    cout << quard(0, 0, n) << '\n';
     return 0;
-
 }
