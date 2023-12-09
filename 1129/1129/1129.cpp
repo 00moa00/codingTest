6e394ed047f1c3452223dc727c04cc3e668c787a
ji young park <moa00_@naver.com>
2023-12-09 22:43:48 +0900

    빈도 정렬 2910

--- 1129/1129/1129.cpp
+++ 1129/1129/1129.cpp
@@ -3,54 +3,67 @@
 
 #include <iostream>
 #include <algorithm>
-#include <string>
+//#include <string>
 #include <vector>
-#include <stack>
-#include <queue>
-#include <tuple>
+#include <map>
+#include <unordered_map>
+//#include <stack>
+//#include <queue>
+//#include <tuple>
 
 using namespace std;
 
-int n, m, j, temp, l, r,ret = 0;
+int c, n, input, cnt;
+unordered_map<int,int> ret;
+map<int, int> pu;
 
 
+bool compareByValue(const pair<int, int>& a, const pair<int, int>& b) 
+ {
+    if (a.second == b.second)
+    {
+        return pu[a.first] < pu[b.first];
 
+    }
+    return a.second > b.second;
+}
 int main() 
 {
     ios_base::sync_with_stdio(0); cin.tie(NULL);
-    cin >> n >> m >> j;
-    l = 1;
+    cin >> n >> c;
 
-    for (int i = 0; i <= j; i++)
+
+    for (int i = 0; i < n; i++)
     {
-        //콜리전 생각하면 된다
-        r = l + m - 1;
-        cin >> temp;
-
-        //범위체크
-        if (temp >= l && temp <= r)
+        cin >> input;
+        if (ret.find(input) == ret.end())
         {
-            continue;
+            ret.insert({ input,1 });
         }
-        
-        else 
+        else
         {
-            //temp가 left보다 왼쪽에 있다. 
-            if (temp < l) 
-            {
-                ret += (l - temp);
-                l = temp;
-            }
+            ret[input] = ret[input] += 1;
+        }
+     
+        if (pu.find(input) == pu.end())
+        {
+            cnt += 1;
+            pu[input] = cnt;
+        }
 
-            //temp가 right보다 오른쪽에 있다. 
-            else
-            {
-                l += (temp - r);
-                ret += (temp - r);
-            }
+    }
+
+    vector<pair<int, int>> sortedRet(ret.begin(), ret.end());
+    sort(sortedRet.begin(), sortedRet.end(), compareByValue);
+
+    for (const auto& pair : sortedRet)
+    {
+        int max_count = pair.second;
+        for (int j = 0; j < max_count; j++) 
+        {
+            cout << pair.first << '\n';
         }
     }
 
-    cout << ret << "\n";
     return 0;
 }
