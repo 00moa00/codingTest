36e1ec234fffac2b991a84534709dca3458ff99a
ji young park <moa00_@naver.com>
2023-12-10 22:00:20 +0900

    비밀번호 발음하기 4659

--- 1129/1129/1129.cpp
+++ 1129/1129/1129.cpp
@@ -1,6 +1,4 @@
-﻿// 1129.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿
 #include <iostream>
 #include <algorithm>
 //#include <string>
@@ -10,58 +8,101 @@
 //#include <stack>
 //#include <queue>
 //#include <tuple>
+#include <cctype>
 
 using namespace std;
 
-int c, n, input, cnt;
-unordered_map<int,int> ret;
-map<int, int> pu;
+int vcnt;   //모음
+int ccnt;   //자음
+bool v = false;     //모음 들어갔는지 체크
+
+bool ret = true;
+
+string InputStr;
 
 
-bool compareByValue(const pair<int, int>& a, const pair<int, int>& b) 
- {
-    if (a.second == b.second)
+bool vCheck(char str)
+{
+    if (str == 'a' || str == 'e' || str == 'i' || str == 'o' || str == 'u')
     {
-        return pu[a.first] < pu[b.first];
-
+        return true;
     }
-    return a.second > b.second;
+    else
+    {
+        return false;
+    }
 }
+
 int main() 
 {
     ios_base::sync_with_stdio(0); cin.tie(NULL);
-    cin >> n >> c;
 
-
-    for (int i = 0; i < n; i++)
+    while (true)
     {
-        cin >> input;
-        if (ret.find(input) == ret.end())
+        cin >> InputStr;
+        if (InputStr == "end")
         {
-            ret.insert({ input,1 });
+            break;
+        }
+
+        //초기화
+        vcnt = 0;
+        ccnt = 0;
+        ret = true;
+        v = false;
+
+        for (int i = 0; i < InputStr.size(); i++)
+        {
+            //소문자로 바꿔주는 함수
+            if (vCheck(tolower(InputStr[i])))
+            {
+                v = true;
+                ccnt = 0;
+                vcnt += 1;
+
+            }
+            else
+            {
+                vcnt = 0;
+                ccnt += 1;
+            }
+
+            //모음 혹은 자음이 연속 3번으로 들어오면 false
+            if (ccnt >= 3 || vcnt >= 3)
+            {
+                ret = false;
+                break;
+            }
+
+            //3. 연속된 같은 문자열 체크
+            if (i > 0 && InputStr[i] == InputStr[i - 1])
+            {
+                string checkStr3 = (i > 1) ? string(1, InputStr[i - 2]) : " ";
+                string check = string(1, InputStr[i]) + InputStr[i - 1] + checkStr3;
+
+                if (check.find("ee") != std::string::npos || check.find("oo") != std::string::npos)
+                {
+                    if (check == ("eee") || check == ("ooo"))
+                    {
+                        ret = false;
+                        break;
+                    }
+
+                    continue;
+                }
+
+                ret = false;
+                break;
+            }
+        }
+
+        if (ret == false || v == false)
+        {
+            cout << "<" << InputStr << ">" << " is not acceptable.\n";
         }
         else
         {
-            ret[input] = ret[input] += 1;
-        }
-     
-        if (pu.find(input) == pu.end())
-        {
-            cnt += 1;
-            pu[input] = cnt;
-        }
-
-    }
-
-    vector<pair<int, int>> sortedRet(ret.begin(), ret.end());
-    sort(sortedRet.begin(), sortedRet.end(), compareByValue);
-
-    for (const auto& pair : sortedRet)
-    {
-        int max_count = pair.second;
-        for (int j = 0; j < max_count; j++) 
-        {
-            cout << pair.first << '\n';
+            cout << "<" << InputStr << ">" << " is acceptable.\n";
         }
     }
 
