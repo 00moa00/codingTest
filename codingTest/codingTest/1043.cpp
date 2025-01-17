f7c4188eca090f0a6b250894d87c951d16fb5d4a
ji young park <moa00_@naver.com>
2025-01-17 17:39:52 +0900

    sw 풀이중

--- codingTest/codingTest/1043.cpp
+++ codingTest/codingTest/1043.cpp
@@ -7,106 +7,106 @@
 using namespace std;
 
 
-int main()
-{
-
-	int n;	//사람 수
-	int m;	//파티 수
-
-	cin >> n >> m;
-
-	//블랙리스트
-	vector<bool> blacklist(n, false);
-
-
-	//진실을 알고있는 사람의 수
-	int t = 0;
-	cin >> t;
-
-	//진실을 알고있는 사람 체크
-	queue<int> q;
-
-	for (int i = 0; i < t; ++i)
-	{
-		int person = 0;
-		cin >> person;
-		blacklist[person - 1] = true;
-		q.push(person - 1);
-	}
-
-	//파티, 파티에 참여한 사람
-	vector<vector<int>> guestlist(m);
-	vector<vector<int>> parties(n); // 각 사람이 참여하는 파티
-
-	//파티 수 만큼 진행
-	for (int i = 0; i < m; ++i)
-	{
-		int quest= 0;
-		cin >> quest;
-		bool bIsblack = false;
-
-		for (int j = 0; j < quest; ++j)
-		{
-			int w = 0;
-			cin >> w;
-			guestlist[i].push_back(w-1);
-			parties[w - 1].push_back(i);
-		}
-
-	}
-
-	// 진실 전파 (BFS)
-	vector<bool> visitedParty(m, false);
-	while (!q.empty())
-	{
-		int person = q.front();
-		q.pop();
-
-		//사람이 참여한 파티
-		for (int party : parties[person])
-		{
-			if (visitedParty[party] == true)
-			{
-				continue; // 이미 처리한 파티는 건너뛰기
-			}
-
-			visitedParty[party] = true;
-
-			//해당 파티에 참여한 게스트 목록
-			for (int guest : guestlist[party]) 
-			{
-				if (blacklist[guest] == false) 
-				{
-
-					//블랙리스트에 등록한다
-					blacklist[guest] = true;
-					q.push(guest);
-				}
-			}
-		}
-	}
-	int ret = 0;
-
-	for (auto party : guestlist)
-	{
-		bool able = true;
-
-		for (auto list : party)
-		{
-			if (blacklist[list] == true)
-			{
-				able = false;
-				break;
-			}
-		}
-
-		if (able == true)
-		{
-			ret++;
-		}
-	}
-
-
-	cout << ret;
-
-}
+//int main()
+//{
+//
+//	int n;	//사람 수
+//	int m;	//파티 수
+//
+//	cin >> n >> m;
+//
+//	//블랙리스트
+//	vector<bool> blacklist(n, false);
+//
+//
+//	//진실을 알고있는 사람의 수
+//	int t = 0;
+//	cin >> t;
+//
+//	//진실을 알고있는 사람 체크
+//	queue<int> q;
+//
+//	for (int i = 0; i < t; ++i)
+//	{
+//		int person = 0;
+//		cin >> person;
+//		blacklist[person - 1] = true;
+//		q.push(person - 1);
+//	}
+//
+//	//파티, 파티에 참여한 사람
+//	vector<vector<int>> guestlist(m);
+//	vector<vector<int>> parties(n); // 각 사람이 참여하는 파티
+//
+//	//파티 수 만큼 진행
+//	for (int i = 0; i < m; ++i)
+//	{
+//		int quest= 0;
+//		cin >> quest;
+//		bool bIsblack = false;
+//
+//		for (int j = 0; j < quest; ++j)
+//		{
+//			int w = 0;
+//			cin >> w;
+//			guestlist[i].push_back(w-1);
+//			parties[w - 1].push_back(i);
+//		}
+//
+//	}
+//
+//	// 진실 전파 (BFS)
+//	vector<bool> visitedParty(m, false);
+//	while (!q.empty())
+//	{
+//		int person = q.front();
+//		q.pop();
+//
+//		//사람이 참여한 파티
+//		for (int party : parties[person])
+//		{
+//			if (visitedParty[party] == true)
+//			{
+//				continue; // 이미 처리한 파티는 건너뛰기
+//			}
+//
+//			visitedParty[party] = true;
+//
+//			//해당 파티에 참여한 게스트 목록
+//			for (int guest : guestlist[party]) 
+//			{
+//				if (blacklist[guest] == false) 
+//				{
+//
+//					//블랙리스트에 등록한다
+//					blacklist[guest] = true;
+//					q.push(guest);
+//				}
+//			}
+//		}
+//	}
+//	int ret = 0;
+//
+//	for (auto party : guestlist)
+//	{
+//		bool able = true;
+//
+//		for (auto list : party)
+//		{
+//			if (blacklist[list] == true)
+//			{
+//				able = false;
+//				break;
+//			}
+//		}
+//
+//		if (able == true)
+//		{
+//			ret++;
+//		}
+//	}
+//
+//
+//	cout << ret;
+//
+//}
---
+++ codingTest/codingTest/solution.cpp
@@ -0,0 +1,244 @@
+﻿#include <vector>
+
+using namespace std;
+
+vector<vector<int>> m;
+
+//검사용 변수
+vector<pair<int, int>> comboblocks;
+
+int wSize = 0;
+int hSize = 0;
+
+int p1Score = 0;
+int p2Score = 0;
+
+//테스트 케이스에 대한 초기화하는 함수. 각 테스트 케이스의 최초 1회 호출된다.
+//W와 H는 각각 게임판의 폭과 높이이다.
+//초기 게임판에는 어떤 블록도 없다.
+//게임판에서 맨 왼쪽 열은 열 0이고 맨 오른쪽 열은 열 "W – 1"이다.
+
+void init(int W, int H)
+{
+    wSize = W;
+    hSize = H;
+
+    m = vector<vector<int>>(H, vector<int>(W, 0));
+}
+
+void removeBlocks(vector<vector<int>>& removeBlock, int dx, int dy, int player)
+{
+    for (int i = 0; i < hSize; ++i)
+    {
+        for (int j = 0; j < wSize; ++j)
+        {
+            comboblocks.clear();
+
+            for (int k = 0;; ++k)
+            {
+                int x = i + k * dx;
+                int y = j + k * dy;
+
+                if (x >= hSize || y >= wSize || x < 0 || y < 0)
+                {
+                    break;
+                }
+
+                if (m[x][y] == player)
+                {
+                    comboblocks.push_back({ x, y });
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (comboblocks.size() >= 5)
+            {
+                for (auto& b : comboblocks)
+                {
+                    removeBlock[b.first][b.second] = player;
+                }
+            }
+        }
+    }
+}
+
+void applyGravity()
+{
+    for (int col = 0; col < wSize; ++col)
+    {
+        int writeRow = hSize - 1;
+
+        for (int row = hSize - 1; row >= 0; --row)
+        {
+            if (m[row][col] != 0)
+            {
+                m[writeRow--][col] = m[row][col];
+            }
+        }
+
+        while (writeRow >= 0)
+        {
+            m[writeRow--][col] = 0;
+        }
+    }
+}
+
+int getScore(int mPlayer)
+{
+    int ret = 0;
+
+    while (true)
+    {
+        vector<vector<int>> removeBlock(hSize, vector<int>(wSize, 0));
+
+        removeBlocks(removeBlock, 0, 1, mPlayer);
+        removeBlocks(removeBlock, 1, 1, mPlayer);
+        removeBlocks(removeBlock, 1, 0, mPlayer);
+
+        bool hasRemoved = false;
+        for (int i = 0; i < hSize; ++i)
+        {
+            for (int j = 0; j < wSize; ++j)
+            {
+                if (removeBlock[i][j] == mPlayer)
+                {
+                    m[i][j] = 0;
+                    ++ret;
+                    hasRemoved = true;
+                }
+            }
+        }
+
+        if (!hasRemoved)
+        {
+            break;
+        }
+
+        applyGravity();
+    }
+
+    if (mPlayer == 1)
+    {
+        p1Score += ret;
+    }
+    else if (mPlayer == 2)
+    {
+        p2Score += ret;
+    }
+
+    return ret;
+}
+
+int dropBlocks(int mPlayer, int mCol)
+{
+    for (int offset = 0; offset < 3; ++offset)
+    {
+        int col = mCol + offset;
+        for (int row = hSize - 1; row >= 0; --row)
+        {
+            if (m[row][col] == 0)
+            {
+                m[row][col] = mPlayer;
+                break;
+            }
+        }
+    }
+
+    return getScore(mPlayer);
+}
+
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+
+vector<vector<int>> visited;
+
+void dfs(int y, int x, int opponentPlayer, int player)
+{
+    visited[y][x] = 1;
+    m[y][x] = player;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || nx < 0 || ny >= hSize || nx >= wSize || visited[ny][nx] || m[ny][nx] != opponentPlayer)
+        {
+            continue;
+        }
+
+        dfs(ny, nx, opponentPlayer, player);
+    }
+}
+
+int changeBlocks(int mPlayer, int mCol)
+{
+    visited = vector<vector<int>>(hSize, vector<int>(wSize, 0));
+
+    int opponent = (mPlayer == 1) ? 2 : 1;
+
+    if (m[hSize - 1][mCol] == opponent)
+    {
+        dfs(hSize - 1, mCol, opponent, mPlayer);
+    }
+
+    return getScore(mPlayer);
+}
+
+int getResult(int mBlockCnt[2])
+{
+    mBlockCnt[0] = 0;
+    mBlockCnt[1] = 0;
+
+    for (int i = 0; i < hSize; ++i)
+    {
+        for (int j = 0; j < wSize; ++j)
+        {
+            if (m[i][j] == 1)
+            {
+                mBlockCnt[0]++;
+            }
+            else if (m[i][j] == 2)
+            {
+                mBlockCnt[1]++;
+            }
+        }
+    }
+
+    if (p1Score > p2Score)
+    {
+        return 1;
+    }
+    else if (p1Score < p2Score)
+    {
+        return 2;
+    }
+    return 0;
+}
+
+
+
+int main()
+{
+    init(10, 10);  // 예시로 10x10 보드 초기화
+
+    // 예시 입력
+    dropBlocks(1, 4);
+    dropBlocks(2, 6);
+    dropBlocks(1, 1);
+    dropBlocks(2, 4);
+    dropBlocks(1, 3);
+    dropBlocks(2, 2);
+    dropBlocks(1, 7);
+    dropBlocks(2, 5);
+
+    // 보드 출력
+    //printBoard();
+    //cout << "Player 1 Score: " << p1Score << endl;
+    //cout << "Player 2 Score: " << p2Score << endl;
+
+    return 0;
+}
