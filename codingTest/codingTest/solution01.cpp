94dcb05a742d14282f9166ebc6d1be790b83b913
ji young park <moa00_@naver.com>
2025-01-20 06:40:51 +0900

    10문제중 4개 통과

--- codingTest/codingTest/solution01.cpp
+++ codingTest/codingTest/solution01.cpp
@@ -11,14 +11,21 @@ int b[MAX_N][MAX_N];
 int bSize = 0;
 int mnSize = 0;
 
+
+// 보드에서 '1' 좌표를 미리 저장
+int treasureCoords[MAX_N * MAX_N][2];
+int treasureCount = 0;
+
 struct Result {
     int y, x;
 };
 
 
 
-//최적화 : 보드에서 상자(1)를 찾아서 조각지도의 9를 기준으로 회전시킨다. 그리고 [조각 지도]를 기준으로 일치하는지 확인한다. 
+//최적화 1 : 보드에서 상자(1)를 찾아서 조각지도의 9를 기준으로 회전시킨다. 그리고 [조각 지도]를 기준으로 일치하는지 확인한다. 
 // FS를 통해 회전된 조각지도가 해당 좌표를 중심으로 보드와 일치하는지 확인한다.
+
+//최적화 2 : 동적할당 삭제, 보드에서 상자(1)가 있는 좌표를 미리 저장
 
 void init(int N, int M, int Map[MAX_N][MAX_N]) 
 {
@@ -27,6 +34,21 @@ void init(int N, int M, int Map[MAX_N][MAX_N])
     for (int i = 0; i < bSize; ++i) 
     {
         memcpy(b[i], Map[i], sizeof(int) * bSize);
+    }
+
+    // '1' 좌표 탐색
+    treasureCount = 0;
+    for (int i = 0; i < bSize; ++i)
+    {
+        for (int j = 0; j < bSize; ++j)
+        {
+            if (b[i][j] == 1)
+            {
+                treasureCoords[treasureCount][0] = i; // y 좌표
+                treasureCoords[treasureCount][1] = j; // x 좌표
+                ++treasureCount;
+            }
+        }
     }
 }
 
@@ -71,60 +93,53 @@ bool isMatch(int y, int x, int Pieces[MAX_M][MAX_M])
 
 Result findTreasureChest(int Pieces[MAX_M][MAX_M])
 {
-    vector<pair<int, int>> treasureCoords;
 
-    // 보드에서 '1' 좌표 수집
-    for (int i = 0; i < bSize; ++i) {
-        for (int j = 0; j < bSize; ++j) {
-            if (b[i][j] == 1) {
-                treasureCoords.push_back(make_pair(i, j));
-            }
-        }
-    }
 
     int rotatedPieces[MAX_M][MAX_M];
     memcpy(rotatedPieces, Pieces, sizeof(int) * MAX_M * MAX_M);
 
     for (int rotation = 0; rotation < 4; ++rotation)
     {
-        // 조각의 '9' 좌표 찾기
-        pair<int, int> pivot = make_pair(-1, -1);
-        for (int i = 0; i < mnSize; ++i) {
+        int pivotY = -1, pivotX = -1;
+
+        for (int i = 0; i < mnSize; ++i)
+        {
             for (int j = 0; j < mnSize; ++j)
             {
                 if (rotatedPieces[i][j] == 9)
                 {
-                    pivot = make_pair(i, j);
+                    pivotY = i;
+                    pivotX = j;
                     break;
                 }
             }
-            if (pivot.first != -1)
+            if (pivotY != -1)
             {
                 break;
             }
             
         }
 
-        for (const auto& coord : treasureCoords)
+        for (int i = 0; i < treasureCount; ++i) 
         {
-            int ty = coord.first, tx = coord.second;
+            int treasureY = treasureCoords[i][0];
+            int treasureX = treasureCoords[i][1];
 
-            // '9'를 중심으로 보정된 좌표
-            int startY = ty - pivot.first;
-            int startX = tx - pivot.second;
+            // 보정된 시작 좌표
+            int startY = treasureY - pivotY;
+            int startX = treasureX - pivotX;
 
             if (startY < 0 || startX < 0 || startY + mnSize > bSize || startX + mnSize > bSize)
             {
                 continue;
-
             }
-
 
             if (isMatch(startY, startX, rotatedPieces))
             {
-                return { ty, tx };
+                return { treasureY, treasureX };
             }
         }
+
 
         // 조각지도 회전
         int temp[MAX_M][MAX_M];
