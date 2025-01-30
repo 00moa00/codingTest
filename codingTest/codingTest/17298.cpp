a36b81135639eb1f2876237d842fe5d658169d4b
ji young park <moa00_@naver.com>
2025-01-31 05:25:01 +0900

    17298 오큰수 다른 풀이

--- codingTest/codingTest/17298.cpp
+++ codingTest/codingTest/17298.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int a[1000000] = { 0, };
-int ret[1000000] = { 0, };
+int ret[1000000] = { -1, };
 
 
 int main()
@@ -16,33 +16,24 @@ int main()
 	cin >> n;
 
 
+	stack<int> s;
+
+	memset(ret, -1, sizeof(ret));
+
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> a[i];
-	}
 
-	stack<int> s;
-
-	for (int i = n - 1; i >= 0; --i)
-	{
-		//현재값보다 작은 수 삭제.
-		//현재값을 어차피 넣을거고, 현재값(가장 왼쪽)보다 작은 값들은 오큰수에서 의미가 없음. 
-		while (s.empty() == false && s.top() <= a[i])
+		//내가 이전의 인덱스보다 크면 내가 오큰수
+		while (s.empty() == false() && a[s.top()] < a[i])
 		{
+			//결과값이 정해졌으니 스택에서 제거해준다.
+			ret[s.top()] = a[i];
 			s.pop();
 		}
-		
-		if (s.empty() == true)
-		{
-			ret[i] = - 1;
-		}
 
-		else
-		{
-			ret[i] = s.top();
-		}
-
-		s.push(a[i]);
+		//아직 정해지지 않은 인덱스 저장
+		s.push(i);
 	}
 
 
